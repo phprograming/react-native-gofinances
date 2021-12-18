56d5d53f70a953d8ae3e58fb72fd202a46fa36e6
Henrique Barbosa <henrique.santos@protonsistemas.com.br>
2021-12-18 10:05:13 -0300

    Initial commit

---
+++ src/components/HighlightCard/index.tsx
@@ -0,0 +1,44 @@
+import React from 'react';
+import { 
+    Container,
+    Header,
+    Title,
+    Icon,
+    Footer,
+    Amount,
+    LastTransation,
+} from './styles';
+
+interface Props {
+    type: 'up' | 'down' | 'total';
+    title: string;
+    amount: string;
+    lastTransaction: string;
+}
+
+const icon = {
+    up: 'arrow-up-circle',
+    down: 'arrow-down-circle',
+    total: 'dollar-sign'
+}
+
+export function HighlightCard({ type, title, amount, lastTransaction } : Props){
+    return (
+        <Container type={type} >
+            <Header>
+                <Title type={type}>
+                    { title }
+                </Title>
+                <Icon name={icon[type]} type={type}/>
+            </Header>
+            <Footer>
+                <Amount type={type}>
+                    { amount }
+                </Amount>
+                <LastTransation type={type}>
+                    { lastTransaction }
+                </LastTransation>
+            </Footer>
+        </Container>
+    )
+}
